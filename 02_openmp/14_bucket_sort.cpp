1417dbbd296d817b86b4f36429321233bad732f9
Ryogo-san <ritosetsugekka0226@gmail.com>
2022-04-19 16:34:47 +0900

    bucket sort

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -13,16 +13,33 @@ int main() {
   printf("\n");
 
   std::vector<int> bucket(range,0); 
+
+  // バケツ作成
+#pragma omp parallel for
   for (int i=0; i<n; i++)
+#pragma omp atomic update
     bucket[key[i]]++;
+
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+  std::vector<int> tmp(range,0);
+
+#pragma omp parallel for
+  for (int i=1;i<range;i++){
+#pragma omp parallel for
+      for(int j=i-1;j>=0;j--) tmp[i]+=bucket[j];
+  }
+
+#pragma omp parallel for
+  for (int i=1;i<range;i++)
+   offset[i]=tmp[i];
+
 #pragma omp parallel for
   for (int i=0; i<range; i++) {
     int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
+#pragma omp parallel for shared(j)
+    for (int k=bucket[i]; k>0; k--) {
       key[j] = i;
+#pragma omp atomic update
       j+=1;
     }
   }
